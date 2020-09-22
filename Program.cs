b5c218163a257b85f121e3c57d773c2ef0b284be
Marce1298 <marchebordon@gmail.com>
2020-09-22 20:55:53 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Metodos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(sumaNumeros(7, 9));
+
+            Console.WriteLine(divideNumeros(18,7));
+
+        }
+
+        static int sumaNumeros(int num1, int num2)
+        {
+
+            //Console.WriteLine($"La suma de los números es {num1 + num2}");
+            return num1 + num2;
+
+        }
+
+        static double divideNumeros(double num1, int num2) => num1 / num2;
+        /*cuando el metodo tiene unicamente una instrucion return (una linea de codigo)
+        puede ser sustituido por ( => )*/
+    
+
+    }
+}
